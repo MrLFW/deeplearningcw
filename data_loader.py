044bae996152b51d188673298a7c09d62c1291ff
Luke Wilson <nk002734@student.reading.ac.uk>
2024-11-23 16:34:18 +0000

    updated data loader and commit before rerunning complete cnn file

--- data_loader.py
+++ data_loader.py
@@ -3,36 +3,7 @@ import numpy as np
 import tensorflow as tf
 
 
-def load_images_and_labels(directory, img_height, img_width, num_classes=101):
-    images = []
-    labels = []
-    class_names = sorted(os.listdir(directory))[-num_classes:]
-    class_indices = {
-        class_name: idx for idx, class_name in enumerate(class_names)
-    }  # {name: index}
-
-    for class_name in class_names:
-        class_dir = os.path.join(
-            directory, class_name
-        )  # caltech-101/101_ObjectCategories/class_name
-        if os.path.isdir(class_dir):
-            for img_name in os.listdir(class_dir):
-                img_path = os.path.join(
-                    class_dir, img_name
-                )  # caltech-101/101_ObjectCategories/class_name/img_name
-                img = tf.keras.preprocessing.image.load_img(
-                    img_path, target_size=(img_height, img_width)
-                )
-                img_array = tf.keras.preprocessing.image.img_to_array(img)
-                images.append(img_array)
-                labels.append(class_indices[class_name])
-
-    return np.array(images), np.array(labels), class_names
-
-
-def load_images_and_labels_without_outliers(
-    directory, img_height, img_width, exclude_classes
-):
+def load_images_and_labels(directory, img_height, img_width, exclude_classes=set()):
     images = []
     labels = []
     class_names = sorted(set(os.listdir(directory)) - exclude_classes)
