39a29f494e90d47b27a65d979504d5988f153642
“nk002734” <nk002734@student.reading.ac.uk>
2024-11-26 22:16:10 +0000

    Revert "fucking mess"
    This reverts commit 2ff1e15d5c2efa53bc54a15b3b7de862adccb40a.

--- data_loader.py
+++ data_loader.py
@@ -1,25 +1,30 @@
 import os
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import tensorflow as tf
 
 
 def load_images_and_labels(directory, img_height, img_width, exclude_classes=set()):
     images = []
     labels = []
     class_names = sorted(set(os.listdir(directory)) - exclude_classes)
-    class_indices = {class_name: idx for idx, class_name in enumerate(class_names)}
+    class_indices = {
+        class_name: idx for idx, class_name in enumerate(class_names)
+    }  # {name: index}
 
     for class_name in class_names:
-        class_dir = os.path.join(directory, class_name)
+        class_dir = os.path.join(
+            directory, class_name
+        )  # caltech-101/101_ObjectCategories/class_name
         if os.path.isdir(class_dir):
             for img_name in os.listdir(class_dir):
-                img_path = os.path.join(class_dir, img_name)
-                img = load_img(img_path, target_size=(img_height, img_width))
-                img_array = img_to_array(img)
+                img_path = os.path.join(
+                    class_dir, img_name
+                )  # caltech-101/101_ObjectCategories/class_name/img_name
+                img = tf.keras.preprocessing.image.load_img(
+                    img_path, target_size=(img_height, img_width)
+                )
+                img_array = tf.keras.preprocessing.image.img_to_array(img)
                 images.append(img_array)
                 labels.append(class_indices[class_name])
 
-    images = np.array(images, dtype=np.float16)
-    labels = np.array(labels)
-
-    return images, labels, class_names
+    return np.array(images), np.array(labels), class_names
